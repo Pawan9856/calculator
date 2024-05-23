e32d75a0a6d49b2def9593cf049bbd939807efcd
Pawan9856 <pawanpaliwal4444@gmail.com>
2024-05-23 16:57:58 +0530

    css and evalution logic added

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { Calculator } from "./components/Calculator";
-import "./App.css";
+// import "./App.css";
 
 function App() {
   return (
     <>
-      <h1>calculator</h1>
       <Calculator />
     </>
   );
